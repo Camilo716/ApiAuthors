9232888b5c5458bb8f6682fd40066c7c1d42a79f
Camilo716 <camilo.gonzalez@praxedes.co>
2023-07-27 15:45:12 -0500

    Refactor: Extract configurations into Startup class

---
+++ ApiAuthors/Startup.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Builder;
+
+namespace ApiAuthors;
+
+public class Startup
+{
+    public IConfiguration _config { get; }
+
+    public Startup(IConfiguration config)
+    {
+        _config = config;
+    }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddControllers();
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen();
+    }
+
+    public void ConfigureMiddlewares(IApplicationBuilder app, IHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseRouting();
+
+        app.UseAuthorization();
+
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllers();
+        });
+    }
+}
+
