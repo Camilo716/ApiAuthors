cf6f70732a66753fe974cc60d3677e078dd469b9
Camilo716 <camilo.gonzalez@praxedes.co>
2023-07-28 15:26:15 -0500

    Update .gitignore

---
+++ ApiAuthors/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using ApiAuthors.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiAuthors;
+
+public class ApplicationDbContext : DbContext
+{
+    public DbSet<AuthorModel> Authors{ get; set; }
+
+    public ApplicationDbContext(DbContextOptions options) : base(options)
+    {
+    }
+}
+
