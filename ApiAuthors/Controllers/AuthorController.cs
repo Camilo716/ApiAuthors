6983068861a393a7dc4c82939757ab29274ae246
camilogs <camilo.gonzalez.col@gmail.com>
2023-07-31 16:19:21 -0500

    feat: Create Put request

--- ApiAuthors/Controllers/AuthorController.cs
+++ ApiAuthors/Controllers/AuthorController.cs
@@ -5,7 +5,7 @@ using Microsoft.EntityFrameworkCore;
 namespace ApiAuthors.Controllers;
 
 [ApiController]
-[Route("[controller]")]
+[Route("api/[controller]")]
 public class AuthorController : ControllerBase
 {
     private ApplicationDbContext _context;
@@ -27,6 +27,18 @@ public class AuthorController : ControllerBase
         _context.Add(author);
         await _context.SaveChangesAsync();
         return Ok();
+    }
+
+    [HttpPut("{id:int}")]
+    public async Task<ActionResult> PutAuthors(AuthorModel author, int id)
+    {
+        if (author.Id != id)
+            return BadRequest("Author's doesn't match with URL's id");
+
+        _context.Update(author);
+        await _context.SaveChangesAsync();
+
+        return Ok();        
     }
 }
 
