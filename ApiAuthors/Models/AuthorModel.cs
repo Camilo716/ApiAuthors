0b490494fcdacfccbb5a4818923476f64053d113
camilogs <camilo.gonzalez.col@gmail.com>
2023-07-31 18:40:21 -0500

    chore: move cycle ignore into startup configurations

--- ApiAuthors/Models/AuthorModel.cs
+++ ApiAuthors/Models/AuthorModel.cs
@@ -1,12 +1,9 @@
-﻿using System.Text.Json.Serialization;
-
-namespace ApiAuthors.Models;
+﻿namespace ApiAuthors.Models;
 
 public class AuthorModel
 {
     public int Id { get; set; }
     public string Name { get; set; }
 
-    [JsonIgnore]
     public List<BookModel> books { get; set; }
 }
--- ApiAuthors/Startup.cs
+++ ApiAuthors/Startup.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using System.Text.Json.Serialization;
+using Microsoft.EntityFrameworkCore;
 
 namespace ApiAuthors;
 
@@ -13,7 +14,9 @@ public class Startup
 
     public void ConfigureServices(IServiceCollection services)
     {
-        services.AddControllers();
+        services.AddControllers()
+                .AddJsonOptions(
+                    x => x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
 
         services.AddDbContext<ApplicationDbContext>(options => 
             options.UseSqlServer(_config.GetConnectionString("dockerConnection")));
