79b3200681fc691c1d65b535146efe1b0f6272d0
camilogs <camilo.gonzalez.col@gmail.com>
2023-08-09 15:13:34 -0500

    Add Microsoft.EntityFrameworkCore.Relational dependence

--- ApiAuthors/Models/BookModel.cs
+++ ApiAuthors/Models/BookModel.cs
@@ -1,9 +1,13 @@
 
+using System.ComponentModel.DataAnnotations;
+
 namespace ApiAuthors.Models;
 
 public class BookModel
 {
     public int Id { get; set; }
+
+    [StringLength(maximumLength: 250)]
     public string Tittle { get; set; }
 
     public int AuthorId { get; set; }
