e323debed77bd069bfc4f398abacf78c28e9752a
camilogs <camilo.gonzalez.col@gmail.com>
2023-08-09 16:24:56 -0500

    Create authors data transfer object

--- ApiAuthors/DTOs/AuthorDTO.cs
+++ ApiAuthors/DTOs/AuthorDTO.cs
@@ -1,6 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+using ApiAuthors.Validations;
+
 namespace ApiAuthors.DTOs;
 
 public class AuthorDTO
 {
-    
+    [Required]
+    [StringLength(maximumLength: 40)]
+    [CapitalizedWords]
+    public string Name { get; set; }
 }
