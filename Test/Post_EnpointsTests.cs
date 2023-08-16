be51d488aab37bd937d41b417c18b593cf8380b4
camilogs <camilo.gonzalez.col@gmail.com>
2023-08-16 14:25:23 -0500

    Create test for Success author POST

--- Test/Post_EnpointsTests.cs
+++ Test/Post_EnpointsTests.cs
@@ -1,5 +1,6 @@
 using System.Text;
 using ApiAuthors.DTOs;
+using Microsoft.VisualStudio.TestPlatform.CommunicationUtilities;
 using Newtonsoft.Json;
 
 namespace Test;
@@ -14,9 +15,9 @@ public class Post_EnpointsTests : IClassFixture<WebApplicationFactory<Program>>
     }
 
     [Theory]
-    [InlineData("Camilo gonzalez")]
+    [InlineData("Martin fowler")]
     [InlineData("Name That Meets The Rule Of Caps But Is Too Long")]
-    public async Task Post_AuthorBadRequests(string name)
+    public async Task Post_AuthorBadRequestsTest(string name)
     {
         var client = _factory.CreateClient();
         var author = new AuthorRequestDTO { Name = name };
@@ -26,6 +27,23 @@ public class Post_EnpointsTests : IClassFixture<WebApplicationFactory<Program>>
         var response = await client.PostAsync("/Api/Author", httpContent); 
 
         Assert.Equal("BadRequest", response.StatusCode.ToString());        
+    }
+
+    [Fact]
+    public async Task Post_AuthorSuccessRequestTest()
+    {
+        var client = _factory.CreateClient();
+        var author = new AuthorRequestDTO{ Name = "Martin Fowler" };
+        var jsonContent = JsonConvert.SerializeObject(author);
+        HttpContent httpContent = new StringContent(jsonContent, Encoding.UTF8, "application/json");
+
+        var response = await client.PostAsync("Api/Author", httpContent);
+        
+        var responseContent = await response.Content.ReadAsStringAsync();
+        Assert.True(
+            response.StatusCode.ToString() == "OK",
+            $"Expected status code OK but got {response.StatusCode}, "+
+            $"Response content: {responseContent}");
     }
 }
     
